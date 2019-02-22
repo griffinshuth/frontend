dc339e026582823f62604cfba1dd0498e05c036a
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-02-21 19:42:35 -0700

    small code spacing change; no features added/removed

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import { HashRouter } from 'react-router-dom';
+import {HashRouter as Router } from 'react-router-dom';
 
 
 ReactDOM.render(
-<HashRouter>
+<Router>
   <App />
-</HashRouter>, document.getElementById('root'));
+</Router>, document.getElementById('root'));
 
